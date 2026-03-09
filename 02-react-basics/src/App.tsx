050f07dccbded76e1d6917524f8a5c92ea3f1f3b
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-03-09 17:15:48 +0100

    förklaringar om state

--- 02-react-basics/src/App.tsx
+++ 02-react-basics/src/App.tsx
@@ -1,131 +1,41 @@
-import './App.css'
-// import React from "react"
-import { useState } from 'react';
 import "./assets/App.scss";
+import './App.css'
+import { useState } from 'react';
 
 function App() {
-  //const counter = 0; stateless value, react does not observe its value, it does not react to it
+  //let counter = 0; stateless value, react does not observe its value, it does not react to it
   const [counter, setCounter] = useState(0)
-  const [msg, setMsg] = useState("hi mum")
-  // const [posts, setPosts] = useState<{ id: number; title: string; content: string}[]>([])
-  const [posts, setPosts] = useState([
-		{ id: 1, title: "React Rocks 🎸!", likes: 1337 },
-		{ id: 2, title: "JSX Rocks Even Moar 🤘🏻!", likes: 42 },
-		{ id: 3, title: "Got state? 🚓", likes: 3 },
-	]);
-  const [salary, setSalary] = useState(10)
-  const [showSalary, setShowSalary] = useState(false);
-
-
+  
   const handleBtnClick = () => {
-    console.log("stop it")
-    // counter++ = variabel kommer öka men sidan kommer inte renderas om - you cannot do counter++ as the destructured state is const, 
-    // setCounter(counter++) means counter + 1 and you are once again trying to reassign a const, consider it an advanced type
-    setCounter(counter + 1) //den här tryggar omrendering, men har du console.log() så kommer den göras efteråt (React lägger på kö alla state uppdateringar och omrenderar)
-    console.log(counter)
-    console.log(msg)
-  }
-
-  	const handleChangeSalary = (amount: number) => {
-		if (salary + amount < 5) {
-      setSalary(5);
-			return;
-		}
-    
-    setSalary(prev => prev + amount);
+	  console.log("counter before increase",counter)
+	  // counter++ = variabel kommer öka men sidan kommer inte renderas om - you cannot do counter++ as the destructured state is const, 
+	  // setCounter(counter++) means counter + 1 and you are once again trying to reassign a const, consider it an advanced type
+	  setCounter(counter + 1) //den här tryggar omrendering, men  den görs efteråt (React lägger på kö alla state uppdateringar och omrenderar så har du console.log inuti denna function kommer den inte visa det aktuella värdet. Omrenderingen kommer rendera om allt förutom funktionen (egentligen det som ändrades - där staten används), rätt värde kommer därför visas på console.log utanför funktionen och uppdaterat värde kommer synas i JSX)
+	  //counter++;
+	  console.log("counter after increase",counter)
+	  // console.log(msg)
 	}
-
+	
 	console.log("App is rendering, counter is:", counter);
-
-  return (
-		<div className="container py-2">
-    <div className="counter">
-			<p>Counter: {counter}</p>
-
-			<button className="btn btn-primary" onClick={handleBtnClick}>Click me!</button>
-		</div>
-			<h1>02-react-basics</h1>
-
-			<h2>Counters</h2>
-
-			<hr />
-
-			<p>{msg}</p>
-
-			<button className="btn btn-warning" onClick={ () => setMsg("Hi dad!") }>Hi dad?</button>
-
-			<hr />
-
-			<button
-				className={showSalary
-					? "btn btn-success mb-4"
-					: "btn btn-danger mb-4"
-				}
-				onClick={() => setShowSalary(!showSalary)} //toggle
-			>
-				{showSalary ? "Hide" : "Show"} salary
-			</button>
-
-			{showSalary && (
-				<>
-					<h2>Salary</h2>
-
-					<p>Salary per hour: {salary} &euro;</p>
-
-					{salary < 10 && (
-						<div className="alert alert-warning">
-							You might want to change job?
-						</div>
-					)}
-
-					<div className="buttons">
-						<div className="mb-1">
-							<button
-								className="btn btn-primary btn-lg"
-								onClick={() => handleChangeSalary(1)}
-							>
-								Raise 1 &euro; 🤑
-							</button>
-							<button
-								className="btn btn-warning btn-lg"
-								disabled={salary === 5}
-								onClick={() => handleChangeSalary(-1)}
-							>
-								Decrease 1 &euro; 😢
-							</button>
-						</div>
-
-						<div className="mb-1">
-							<button
-								className="btn btn-primary btn-lg"
-								onClick={() => handleChangeSalary(5)}
-							>
-								Raise 5 &euro; 🤑🤑🤑
-							</button>
-							<button
-								className="btn btn-warning btn-lg"
-								disabled={salary === 5}
-								onClick={() => handleChangeSalary(-5)}
-							>
-								Decrease 5 &euro; 😢😢😢
-							</button>
-						</div>
-					</div>
-				</>
-			)}
-
-			<hr />
-
-			<h2>Posts</h2>
-
-			<ul>
-				{posts.map(post =>
-					<li key={post.id}>{post.title} ({post.likes} likes)</li>
-				)}
-			</ul>
-		</div>
-	);
+	
+	return (
+	<div className="container">
+		<p>Counter: {counter}</p>
+		<button onClick={handleBtnClick} className="btn btn-primary">Click me</button>
+	</div>
+  )
 }
 
 export default App;
 
+
+//   const [counter, setCounter] = useState(0)
+//   const [msg, setMsg] = useState("hi mum")
+  // const [posts, setPosts] = useState<{ id: number; title: string; content: string}[]>([])
+//   const [posts, setPosts] = useState([
+		// { id: 1, title: "React Rocks 🎸!", likes: 1337 },
+		// { id: 2, title: "JSX Rocks Even Moar 🤘🏻!", likes: 42 },
+		// { id: 3, title: "Got state? 🚓", likes: 3 },
+	// ]);
+//   const [salary, setSalary] = useState(10)
+//   const [showSalary, setShowSalary] = useState(false);
