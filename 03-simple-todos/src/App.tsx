0cc737c9f22ce068cefdc597803ae0d088fb43a2
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-03-17 16:20:03 +0100

    todos list

--- 03-simple-todos/src/App.tsx
+++ 03-simple-todos/src/App.tsx
@@ -1,12 +1,24 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import "./assets/App.scss";
 
-function App() {
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean
+}
 
+function App() {
+const [todos, setTodos] = useState<Todo[]>([
+  {id: 1, title: "first to-do", done:false},
+  {id: 2, title: "second to-do", done:false},
+  {id: 3, title: "thirds to-do", done:false},
+])
 
   return (
     <div className="container">
-      <p>Hi</p>
+      {todos.map(todo => <ul>
+        <li>{todo.title}</li>
+      </ul>)}
     </div>
   )
 }
