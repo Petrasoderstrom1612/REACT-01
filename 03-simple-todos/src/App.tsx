30a60304842447a4e7a925c9c3b2b95d71021be3
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-03-18 19:23:01 +0100

    format force and .gitkeep

--- 03-simple-todos/src/App.tsx
+++ 03-simple-todos/src/App.tsx
@@ -1,85 +1,104 @@
-import { useState } from 'react'
+import { useState } from "react";
 import "./assets/App.scss";
-import TodoCounter from './components/TodoCounter';
-import AddNewTodoForm from './components/AddNewTodoForm';
+import TodoCounter from "./components/TodoCounter";
+import AddNewTodoForm from "./components/AddNewTodoForm";
 
 export interface Todo {
   id: number;
   title: string;
-  done: boolean
+  done: boolean;
 }
 
 function App() {
-const [todos, setTodos] = useState<Todo[]>([
-  {id: 1, title: "first to-do", done:false},
-  {id: 2, title: "second to-do", done:false},
-  {id: 3, title: "thirds to-do", done:false},
-])
+  const [todos, setTodos] = useState<Todo[]>([
+    { id: 1, title: "first to-do", done: false },
+    { id: 2, title: "second to-do", done: false },
+    { id: 3, title: "thirds to-do", done: false },
+  ]);
 
-const removeTodo = (clickedId: number) => {
-  setTodos(prevTodos => prevTodos.filter(todo => todo.id !== clickedId))
-}
+  const removeTodo = (clickedId: number) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== clickedId));
+  };
 
-const toggleTodo = (clickedId: number) => {
-  setTodos(prevTodos => prevTodos.map(todo =>
-    todo.id === clickedId ? {...todo, done: !todo.done} : todo
-  ))
-}
+  const toggleTodo = (clickedId: number) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === clickedId ? { ...todo, done: !todo.done } : todo,
+      ),
+    );
+  };
 
-const addTodo = (title:string) => {
+  const addTodo = (title: string) => {
     const newTodo: Todo = {
-    id: Math.max(0, ...todos.map(todo => todo.id)) + 1,
-    title,
-    done: false
-  }
+      id: Math.max(0, ...todos.map((todo) => todo.id)) + 1,
+      title,
+      done: false,
+    };
 
-  setTodos(prevTodos => [...prevTodos, newTodo])
-
-}
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
 
   return (
     <>
-    <h1>Todos</h1>
-    <div className="container">
-      {todos.length === 0 && <p>No todos</p>}
-      {todos.length > 0 && (
-        <>
-          {todos
-          .filter(todo => !todo.done)
-          .map(todo => 
-          <ul key={todo.id}>
-            <li onClick={() => { toggleTodo(todo.id); } } className={todo.done ? "done" : undefined}>
-              {todo.done ? "✅" : undefined} {todo.title}
-              <button
-                type="button"
-                title="trash-bin"
-                className="btn btn-danger ms-3"
-                onClick={() => removeTodo(todo.id)}>🗑️</button>
-            </li>
-          </ul>
+      <h1>Todos</h1>
+      <div className="container">
+        {todos.length === 0 && <p>No todos</p>}
+        {todos.length > 0 && (
+          <>
+            {todos
+              .filter((todo) => !todo.done)
+              .map((todo) => (
+                <ul key={todo.id}>
+                  <li
+                    onClick={() => {
+                      toggleTodo(todo.id);
+                    }}
+                    className={todo.done ? "done" : undefined}
+                  >
+                    {todo.done ? "✅" : undefined} {todo.title}
+                    <button
+                      type="button"
+                      title="trash-bin"
+                      className="btn btn-danger ms-3"
+                      onClick={() => removeTodo(todo.id)}
+                    >
+                      🗑️
+                    </button>
+                  </li>
+                </ul>
+              ))}
+            <hr />
+            {todos
+              .filter((todo) => todo.done)
+              .map((todo) => (
+                <ul key={todo.id}>
+                  <li
+                    onClick={() => {
+                      toggleTodo(todo.id);
+                    }}
+                    className={todo.done ? "done" : undefined}
+                  >
+                    {todo.done ? "✅" : undefined} {todo.title}
+                    <button
+                      type="button"
+                      title="trash-bin"
+                      className="btn btn-danger ms-3"
+                      onClick={() => removeTodo(todo.id)}
+                    >
+                      🗑️
+                    </button>
+                  </li>
+                </ul>
+              ))}
+            <TodoCounter todos={todos} />
+            <hr />
+          </>
         )}
-        <hr/>
-          {todos
-          .filter(todo => todo.done)
-          .map(todo => 
-          <ul key={todo.id}>
-            <li onClick={() => { toggleTodo(todo.id); } } className={todo.done ? "done" : undefined}>
-              {todo.done ? "✅" : undefined} {todo.title}
-              <button
-                type="button"
-                title="trash-bin"
-                className="btn btn-danger ms-3"
-                onClick={() => removeTodo(todo.id)}>🗑️</button>
-            </li>
-          </ul>)}
-          <TodoCounter todos={todos}/>
-          <hr/>
-        </>
-      )}
 
-      <AddNewTodoForm addTodo={addTodo}/>
-    </div></>
-  )
+        <AddNewTodoForm addTodo={addTodo} />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
