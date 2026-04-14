526117029b8e29b59c8cfa3ec492e18304ab10cd
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-04-14 19:05:45 +0200

    format

--- 08-hackernews/src/hooks/useLocalStorage.ts
+++ 08-hackernews/src/hooks/useLocalStorage.ts
@@ -1,7 +1,7 @@
 //custom hook return value or function so NOT tsx!
 import { useState } from "react"; 
 
-//GENERIC
+//GENERIC SAVING
 const useLocalStorage = <T>(key: string, defaultValue: T) => {
 const [storedValue, setStoredValue] = useState<T>(() => {//T for default value
     const value = localStorage.getItem(key) ?? "true" //default value if no storage yet
@@ -19,7 +19,10 @@ const setValue= (newValue: T) => {
 }
 
 //this is the same as const [isDarkMode, setIsDarkMode] that is then used in ThemeContextProvider
-return [storedValue, setValue] as const //we return a constant array otherwise declare up tuple  [boolean, (newValue: boolean) => void]
+return [
+    storedValue, 
+    setValue
+] as const //we return a constant array otherwise declare up tuple  [boolean, (newValue: boolean) => void]
 }
 
 export default useLocalStorage;
