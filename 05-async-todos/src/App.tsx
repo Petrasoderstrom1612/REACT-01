0b1f5970ddd66c990473d7c3e077852a70046426
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2026-04-14 17:31:42 +0200

    prepared for next

--- 05-async-todos/src/App.tsx
+++ 05-async-todos/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/set-state-in-effect */
 import { useEffect, useState } from "react";
 import "./assets/scss/App.scss";
 import TodoCounter from "./components/TodoCounter";
@@ -9,8 +10,21 @@ import TodoList from "./components/TodoList";
 
 
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]); //you have to have type of the array obj
+  const [error, setError] = useState<string | false>(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [todos, setTodos] = useState<Todo[]>([]); //the best practise below
+  //const [todos, setTodos] = useState<Todo[] | null>(null); //if not data return to have a falsy value, empty array would confuse error generating
 
+  const addTodo = (title: string) => {
+    const newTodo: Todo = {
+      id: Math.max(0, ...todos.map((todo) => todo.id)) + 1,
+      title: title,
+      done: false,
+    };
+
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+  
   const removeTodo = (clickedId: number) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== clickedId));
   };
@@ -23,26 +37,25 @@ function App() {
     );
   };
 
-  const addTodo = (title: string) => {
-    const newTodo: Todo = {
-      id: Math.max(0, ...todos.map((todo) => todo.id)) + 1,
-      title: title,
-      done: false,
-    };
-
-    setTodos((prevTodos) => [...prevTodos, newTodo]);
-  };
 
   useEffect( () => { //you cannot have async here as useEffect would return promise and useEffect only wants to return clean up function
     const getData = async () => { //store the fetching into a separate function to obey the row above
-      const res = await fetch("http://localhost:3000/todos") //fetch is inbuild in all browsers nowadays
-      if (!res.ok){ //without .then as we use async
-        throw new Error("res was not ok")
-      }
-      const data: Todo[] = await res.json() // or as Todo[]
-      setTodos(data)
-    }
+      try {
+        const res = await fetch("http://localhost:3000/todos") //fetch is inbuild in all browsers nowadays
+          if (!res.ok){ //without .then as we use async already notes if the server is on or not
+            throw new Error("res was not ok")
+          }
+        const data: Todo[] = await res.json() // or as Todo[]
+        setTodos(data)
 
+      } catch (err) {
+        console.error("get data error:", err)
+        setError(err instanceof Error ? "Could not load:" + err.message : "you did something wrong in code") // err instanceof Error TS
+        console.log(error)
+      } finally{ //you can skip finally and have it after the try if
+        setIsLoading(false)
+      }
+    }
     getData()
   },[])
 
@@ -53,8 +66,9 @@ function App() {
   return (
       <Container>
       <h1>Todos</h1>
-        {todos.length === 0 && <p className="text-muted">No todos</p>}
-        {todos.length && (
+        {isLoading && <p>Loading todos...</p>}
+        {!isLoading && !error && todos.length === 0 && <p className="text-muted">No todos</p>} 
+        {!isLoading && !error && todos.length && ( //very important to ensure you are not in loading state and you do not have error
           <>
           <h2 className="h5 mb-2">Todo items</h2> {/* Show visually h5 but respect screenreaders */}
                 <TodoList todos={incompletedTodos} onRemoveTodo={removeTodo} onToggle={toggleTodo}/>
